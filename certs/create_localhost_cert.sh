4d052245983d98c0b1904604ab7b23a447488efb
Niyamaka <niyamaka@yuneta.io>
2020-06-26 01:00:50 +0200

    self-signed certificates in localhost: remember connect to websocket port from browser to validate 'Warning: Potential Security Risk Ahead'

--- certs/create_localhost_cert.sh
+++ certs/create_localhost_cert.sh
@@ -1,5 +1,5 @@
 # https://stackoverflow.com/questions/10175812/how-to-create-a-self-signed-certificate-with-openssl
 
 /usr/bin/openssl req -x509 -newkey rsa:4096 -sha256 -days 3650 -nodes \
-  -keyout yuneta.key -out yuneta.crt -subj "/CN=yuneta.io" \
-  -addext "subjectAltName=DNS:yuneta.io,DNS:yuneta.com,IP:127.0.0.1"
+  -keyout localhost.key -out localhost.crt -subj "/CN=localhost" \
+  -addext "subjectAltName=DNS:localhost,DNS:localhost,IP:127.0.0.1"
