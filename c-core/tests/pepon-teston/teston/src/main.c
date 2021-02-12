645e79ae661c65db61978823199873eef215b441
Niyamaka <niyamaka@yuneta.io>
2021-02-12 11:17:39 +0100

    pipe inheritance

--- c-core/tests/pepon-teston/teston/src/main.c
+++ c-core/tests/pepon-teston/teston/src/main.c
@@ -17,7 +17,7 @@
 #define APP_NAME        "teston"
 #define APP_DOC         "Teston, yuno client de pruebas"
 
-#define APP_VERSION     "4.9.2"
+#define APP_VERSION     "4.9.3"
 #define APP_SUPPORT     "<niyamaka@yuneta.io>"
 #define APP_DATETIME    __DATE__ " " __TIME__
 
