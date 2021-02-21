47a072e07c479edb6e8d11fa356d9696626cc353
Niyamaka <niyamaka@yuneta.io>
2021-02-21 21:51:08 +0100

    v4.9.8

--- c-core/tests/pepon-teston/pepon/src/main.c
+++ c-core/tests/pepon-teston/pepon/src/main.c
@@ -17,7 +17,7 @@
 #define APP_NAME        "pepon"
 #define APP_DOC         "Pepon, yuno server de pruebas"
 
-#define APP_VERSION     "4.9.7"
+#define APP_VERSION     "4.9.8"
 #define APP_SUPPORT     "<niyamaka@yuneta.io>"
 #define APP_DATETIME    __DATE__ " " __TIME__
 
