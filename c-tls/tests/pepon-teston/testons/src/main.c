e4e3d25b655a1f16e0f876a680d49b64b7cc93b1
Niyamaka <niyamaka@yuneta.io>
2021-09-06 13:57:59 +0200

    v4.16.0 task_authentication does the logout

--- c-tls/tests/pepon-teston/testons/src/main.c
+++ c-tls/tests/pepon-teston/testons/src/main.c
@@ -17,7 +17,7 @@
 #define APP_NAME        ROLE_TESTONS
 #define APP_DOC         "Teston, yuno client de pruebas"
 
-#define APP_VERSION     "4.15.9"
+#define APP_VERSION     "4.16.0"
 #define APP_SUPPORT     "<niyamaka@yuneta.io>"
 #define APP_DATETIME    __DATE__ " " __TIME__
 
