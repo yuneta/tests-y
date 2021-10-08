3e3d9691f197aebfa32f99e2e73b07be496f9fb1
Niyamaka <niyamaka@yuneta.io>
2021-10-08 10:37:23 +0200

    v4.20.3 fix postgres

--- c-core/tests/pepon-teston/teston/src/main.c
+++ c-core/tests/pepon-teston/teston/src/main.c
@@ -17,7 +17,7 @@
 #define APP_NAME        "teston"
 #define APP_DOC         "Teston, yuno client de pruebas"
 
-#define APP_VERSION     "4.20.2"
+#define APP_VERSION     "4.20.3"
 #define APP_SUPPORT     "<niyamaka@yuneta.io>"
 #define APP_DATETIME    __DATE__ " " __TIME__
 
